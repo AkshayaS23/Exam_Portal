15486b7c5df7abd381d547e97fcaad07ed33233d
Akshaya <akshayaviswanathan8@gmail.com>
2025-10-30 14:17:45 +0530

    route updated

--- exam-portal/src/main.jsx
+++ exam-portal/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Exam_Portal">
       <App />
     </BrowserRouter>
   </StrictMode>
